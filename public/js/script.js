18d1b82cba6ad48bff61902b2d224ca704307b58
victorsc45 <victorlitzau@gmail.com>
2020-09-07 12:24:19 -0400

    quote stock working

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function () {
 
     var settings = {
@@ -13,39 +15,72 @@ $(document).ready(function () {
 
     $.ajax(settings).done(function (response) {
         // the S&P 500 Daily information
-        console.log('this is response', response);
         let searchsnp = response.marketSummaryResponse.result[0];
-        console.log("this is the search marketsummar", searchsnp);
         let snp = searchsnp.shortName;
         console.log("this is the name of exchange", snp);
         let SNPprice = searchsnp.regularMarketPrice.fmt;
-        console.log("this is the price of the index", SNPprice);
         let SNPchange = searchsnp.regularMarketChangePercent.fmt;
-        console.log('this is the daily change%', SNPchange);
+        let SNPDATA = `Regular Market Price today:
+        $ ${SNPprice} 
+         Regular Market Change Percent today:
+        ${SNPchange}`;
+        $('[data-toggle="SNPpopover"]').popover({ title: snp, content: SNPDATA });
+
+
+
         // Dow Jones Industrial Information 
-        console.log('this is response Dow', response);
+
         let searchDow = response.marketSummaryResponse.result[1];
-        console.log("this is the search marketsummar", searchDow);
         let dow = searchDow.shortName;
-        console.log("this is the name of exchange", dow);
         let DOWprice = searchDow.regularMarketPrice.fmt;
-        console.log("this is the price of the index", DOWprice);
-        let Dowchange = searchDow.regularMarketChangePercent.fmt;
-        console.log('this is the daily change%', Dowchange);
+        let DOWchange = searchDow.regularMarketChangePercent.fmt;
+        let DOWDATA = `Regular Market Price today:
+        $ ${DOWprice} 
+        Regular Market Change Percent today:
+        ${DOWchange}`;
+        $(`[data-toggle="DOWpopover"]`).popover({ title: dow, content: DOWDATA });
+
+
+
         //Nasdaq composite information 
-        console.log('this is response Dow', response);
+
         let searchNasdaq = response.marketSummaryResponse.result[2];
-        console.log("this is the search marketsummar", searchNasdaq);
         let nasdaq = searchNasdaq.shortName;
-        console.log("this is the name of exchange", nasdaq);
         let NASprice = searchNasdaq.regularMarketPrice.fmt;
-        console.log("this is the price of the index", NASprice);
-        let Naschange = searchNasdaq.regularMarketChangePercent.fmt;
-        console.log('this is the daily change%', Naschange);
+        let NASchange = searchNasdaq.regularMarketChangePercent.fmt;
+        let NASDATA = `Regular Market Price today:
+        $ ${NASprice} 
+        Regular Market Change Percent today:
+        ${NASchange}`;
+        $(`[data-toggle="NASpopover"]`).popover({ title: nasdaq, content: NASDATA });
     });
 
 
+    let symbolBtn = $("#symbolBtn");
+    //button click handler for submitting an artist query for a investment quote
+    symbolBtn.on("click", symbolSubmit);
+    function symbolSubmit(event) {
+        event.preventDefault();
+        symbol = $('#symbol').val().trim();
+        let query = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-quotes?region=US&lang=en&symbols=" + symbol + "%252CKC%253DF%252C002210.KS%252CIWM%252CAMECX",
+            settings = {
+                "async": true,
+                "crossDomain": true,
+                "url": query,
+                "method": "GET",
+                "headers": {
+                    "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
+                    "x-rapidapi-key": "acd626c871msh56e79dd581cd845p1e0ed3jsndbc38000616c"
+                }
+            }
 
+        $.ajax(settings).done(function (SYMBresponse) {
 
+            let quoted = SYMBresponse.quoteResponse.result[0].regularMarketPrice;
+            quoteOut = `Regular Market Price: $ ${quoted.toFixed(2)}`;
+            document.getElementById("quote").value = quoteOut;
+
+        });
+    };
 });
 
