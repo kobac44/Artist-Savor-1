2294cd1341c720cfbde5c6e65f1305100834f13e
victorsc45 <victorlitzau@gmail.com>
2020-09-08 10:40:59 -0400

    preparing for heroku deploy

---
+++ config/config.js
@@ -0,0 +1,27 @@
+// require("dotenv").config();
+
+// module.exports = {
+//     development: {
+//         username: process.env.DB_USER,
+//         password: process.env.DB_PASS,
+//         database: "passport_demo",
+//         host: process.env.DB_HOST,
+//         dialect: "mysql"
+//     },
+//     test: {
+//         username: process.env.DB_USER,
+//         password: process.env.DB_PASS,
+//         database: "passport_demo",
+//         host: "127.0.0.1",
+//         dialect: "mysql",
+//         logging: false
+//     },
+//     production: {
+//         use_env_variable: process.env.JAWSDB_URL,
+//         dialect: "mysql",
+//         username: process.env.DB_USER,
+//         password: process.env.DB_PASS,
+//         database: process.env.DB,
+//         host: process.env.DB_HOST
+//     }
+// };
