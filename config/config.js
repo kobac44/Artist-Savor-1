27feea399295ee73c7791a9d569b5ba8f3839882
victorsc45 <victorlitzau@gmail.com>
2020-09-09 13:19:55 -0400

    attempting config file

--- config/config.js
+++ config/config.js
@@ -2,28 +2,28 @@ require("dotenv").config();
 // require dotenv to local, test, and let jawsdb handle process env
 module.exports = {
     development: {
-        "username": process.env.DB_USER,
-        "password": process.env.DB_PASS,
-        "database": process.env.DB,
-        "host": process.env.DB_HOST,
-        "dialect": "mysql"
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB,
+        host: process.env.DB_HOST,
+        dialect: "mysql"
     },
     test: {
-        "username": process.env.DB_USER,
-        "password": process.env.DB_PASS,
-        "database": process.env.DB,
-        "host": process.env.DB_HOST,
-        "dialect": "mysql",
-        "logging": false
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB,
+        host: process.env.DB_HOST,
+        dialect: "mysql",
+        logging: false
     },
     production: {
 
-        "use_env_variable": process.env.JAWSDB_URL,
-        "dialect": "mysql",
-        // "username": process.env.DB_USER,
-        // "password": process.env.DB_PASS,
-        // "host": process.env.DB_HOST,
-        // "database": process.env.DB
+        use_env_variable: process.env.JAWSDB_URL,
+        dialect: "mysql",
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB,
+        host: process.env.DB_HOST
 
     }
 };                                                       
